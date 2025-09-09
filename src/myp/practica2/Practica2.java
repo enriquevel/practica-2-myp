4ebd01e50d0238bc8a48140821436030ecba87d2
enriquevel <enriquev@ciencias.unam.mx>
2025-09-09 17:18:29 -0600

    Se acabo el menu principal para el usuario.

--- src/myp/practica2/Practica2.java
+++ src/myp/practica2/Practica2.java
@@ -19,12 +19,21 @@ public class Practica2 {
 				case 2:
 					robot.takePizzaOrder();
 					break;
-
-				case 3: // helado
-				case 4: // confirmar
-				case 5: // cancelar
-				case 6: // preparar
-					case 7: // recoger orden
+				case 3:
+					robot.takeIceCreamOrder();
+					break;
+				case 4:
+					robot.confirmOrder();
+					break;
+				case 5: 
+					robot.cancelOrder();
+					break;
+				case 6:
+					robot.prepareOrder();
+					break;
+				case 7:
+					robot.deliverOrder();
+					break;
 				case 0:
 					System.out.println("Vuelve pronto.");
 					System.exit(0);
@@ -32,9 +41,17 @@ public class Practica2 {
 		}
 	}
 
+	/**
+	 * Metodo auxiliar que imprime el menu principal de opciones.
+	 */
 	private static void printOptions() {
 		System.out.println("(1). Llamar robot.");
 		System.out.println("(2). Ordenar pizza.");
-		System.out.println();
+		System.out.println("(3). Ordenar helado.");
+		System.out.println("(4). Confirmar orden.");
+		System.out.println("(5). Cancelar orden.");
+		System.out.println("(6). Preparar orden");
+		System.out.println("(7). Solicitar entrega de orden.");
+		System.out.println("(8). Salir.");
 	}
 }
